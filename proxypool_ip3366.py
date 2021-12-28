c161f65718833474fa27ecbb803eb7a46219a2f4
Sancho <42240228+GitHubSancho@users.noreply.github.com>
2021-12-28 20:55:05 +0800

    Add files via upload
    一个爬取代理IP的爬虫，方便之后爬虫使用。
    之后可能会优化一下验证速度。

---
+++ proxypool_ip3366.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import requests as rq
+from lxml import etree
+import telnetlib as tn
+import pandas as pd
+import time
+
+if __name__ == "__main__":
+    #提取IP
+    url = "https://proxy.ip3366.net/free/"
+    rsp = []
+    for i in range(1, 11):
+        html = rq.get(url=url, params={"action": "china", "page": i})
+        html.encoding = "utf-8"
+        rsp.append(html.text)
+        html.close()
+
+    tbodys = [
+        etree.HTML(i).xpath("/html/body/section/section/div[2]/table/tbody")
+        for i in rsp
+    ]
+
+    ips = pd.concat([
+        pd.DataFrame([
+            body.xpath("./tr/td[1]/text()"),
+            body.xpath("./tr/td[2]/text()"),
+            body.xpath("./tr/td[3]/text()"),
+            body.xpath("./tr/td[4]/text()"),
+            body.xpath("./tr/td[5]/text()")
+        ],
+                     index=["ip", "port", "anmt", "http", "place"]).T
+        for tbody in tbodys for body in tbody
+    ],
+                    axis=0,
+                    ignore_index=True).drop_duplicates().reset_index(drop=True)
+
+    #测试IP
+    pd.set_option('display.unicode.ambiguous_as_wide', True)
+    pd.set_option('display.unicode.east_asian_width', True)
+
+    def ping(self):
+        try:
+            stime = time.time()
+            test = tn.Telnet(self["ip"], port=self["port"], timeout=5)
+            etime = time.time()
+            print(self["ip"], self["port"], "YES")
+            test.close()
+            return etime - stime
+        except:
+            print(self["ip"], self["port"], "NO")
+            return -1
+
+    ips["ping"] = ips.apply(ping, axis=1)
+    ips.sort_values(by="ping", inplace=True, ascending=True)
+    print(ips[ips["ping"] != -1].to_csv("proxypool.csv", index=False))

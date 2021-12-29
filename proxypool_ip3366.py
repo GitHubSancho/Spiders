8ae3038ad97a3d6993030e2e2eaa7118c4397680
Sancho <42240228+GitHubSancho@users.noreply.github.com>
2021-12-29 16:37:17 +0800

    Update proxypool_ip3366.py

--- proxypool_ip3366.py
+++ proxypool_ip3366.py
@@ -5,54 +5,67 @@ from lxml import etree
 import telnetlib as tn
 import pandas as pd
 import time
+from concurrent.futures import ThreadPoolExecutor as tpe
 
-if __name__ == "__main__":
-    #提取IP
-    url = "https://proxy.ip3366.net/free/"
-    rsp = []
-    for i in range(1, 11):
-        html = rq.get(url=url, params={"action": "china", "page": i})
-        html.encoding = "utf-8"
-        rsp.append(html.text)
-        html.close()
 
-    tbodys = [
-        etree.HTML(i).xpath("/html/body/section/section/div[2]/table/tbody")
-        for i in rsp
-    ]
+#提取IP
+def draw(url, i):
+    # 解析网页
+    html = rq.get(url=url, params={"action": "china", "page": i})
+    html.encoding = "utf-8"
+    rsp = html.text
+    html.close()
 
-    ips = pd.concat([
+    # 提取网页数据
+    tbodys = etree.HTML(rsp).xpath(
+        "/html/body/section/section/div[2]/table/tbody")
+    ips = [
         pd.DataFrame([
-            body.xpath("./tr/td[1]/text()"),
-            body.xpath("./tr/td[2]/text()"),
-            body.xpath("./tr/td[3]/text()"),
-            body.xpath("./tr/td[4]/text()"),
-            body.xpath("./tr/td[5]/text()")
+            tbody.xpath("./tr/td[1]/text()"),
+            tbody.xpath("./tr/td[2]/text()"),
+            tbody.xpath("./tr/td[3]/text()"),
+            tbody.xpath("./tr/td[4]/text()"),
+            tbody.xpath("./tr/td[5]/text()")
         ],
                      index=["ip", "port", "anmt", "http", "place"]).T
-        for tbody in tbodys for body in tbody
-    ],
-                    axis=0,
-                    ignore_index=True).drop_duplicates().reset_index(drop=True)
-
-    #测试IP
-    pd.set_option('display.unicode.ambiguous_as_wide', True)
-    pd.set_option('display.unicode.east_asian_width', True)
-
-    def ping(self):
-        try:
-            stime = time.time()
-            test = tn.Telnet(self["ip"], port=self["port"], timeout=5)
-            etime = time.time()
-            print(self["ip"], self["port"], "YES")
-            test.close()
-            return etime - stime
-        except:
-            print(self["ip"], self["port"], "NO")
-            return -1
+        for tbody in tbodys
+    ][0].drop_duplicates()
 
     ips["ping"] = ips.apply(ping, axis=1)
+    return ips
+
+
+# 验证IP
+def ping(self):
+    try:
+        stime = time.time()
+        test = tn.Telnet(self["ip"], port=self["port"], timeout=5)
+        etime = time.time()
+        print(self["ip"], self["port"], "YES")
+        test.close()
+        return etime - stime
+    except:
+        print(self["ip"], self["port"], "NO")
+        return -1
+
+
+if __name__ == "__main__":
+    url = "https://proxy.ip3366.net/free/"
+
+    # 使用线程池
+    ips = []
+    with tpe(10) as pool:
+        for i in range(1, 11):
+            ips.append(pool.submit(draw, url, i))
+    ips = pd.concat([i.result() for i in ips], axis=0,
+                    ignore_index=True).drop_duplicates()
+
+    # 排序和筛选
     ips.sort_values(by="ping", inplace=True, ascending=True)
     ips_ture = ips[ips["ping"] != -1]
-    print(ips_ture)
-    ips_ture.to_csv("proxypool.csv", index=False)
+
+    # 设置输出
+    pd.set_option('display.unicode.ambiguous_as_wide', True)
+    pd.set_option('display.unicode.east_asian_width', True)
+    print(ips_ture.reset_index(drop=True))
+    ips_ture.to_csv("proxypool.csv", index=False)  #输出到文件

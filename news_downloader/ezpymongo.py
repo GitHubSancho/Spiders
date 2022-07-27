cd8ba40c7689272811f7b27d16365a27dede2ac9
Sancho <deerflores@gmail.com>
2022-07-28 03:29:26 +0800

    modified:   news_downloader/ezpymongo.py

--- news_downloader/ezpymongo.py
+++ news_downloader/ezpymongo.py
@@ -16,58 +16,6 @@ from urllib import parse
 
 
 class Connection(object):
-    # def __init__(self,
-    #              host,
-    #              database,
-    #              user=None,
-    #              password=None,
-    #              port=0,
-    #              max_idle_time=7 * 3600,
-    #              connect_timeout=10,
-    #              time_zone="+0:00",
-    #              charset="utf8mb4",
-    #              sql_mode="TRADITIONAL"):
-    #     self.host = host
-    #     self.database = database
-    #     self.max_idle_time = float(max_idle_time)
-
-    #     args = dict(use_unicode=True,
-    #                 charset=charset,
-    #                 database=database,
-    #                 init_command=('SET time_zone = "%s"' % time_zone),
-    #                 cursorclass=pymysql.cursors.DictCursor,
-    #                 connect_timeout=connect_timeout,
-    #                 sql_mode=sql_mode)
-    #     if user is not None:
-    #         args["user"] = user
-    #     if password is not None:
-    #         args["passwd"] = password
-
-    #     # We accept a path to a MySQL socket file or a host(:port) string
-    #     if "/" in host:
-    #         args["unix_socket"] = host
-    #     else:
-    #         self.socket = None
-    #         pair = host.split(":")
-    #         if len(pair) == 2:
-    #             args["host"] = pair[0]
-    #             args["port"] = int(pair[1])
-    #         else:
-    #             args["host"] = host
-    #             args["port"] = 3306
-    #     if port:
-    #         args['port'] = port
-
-    #     self._db = None
-    #     self._db_args = args
-    #     self._last_use_time = time.time()
-    #     try:
-    #         self.reconnect()
-    #     except Exception:
-    #         logging.error("Cannot connect to MySQL on %s",
-    #                       self.host,
-    #                       exc_info=True)
-
     def __init__(self,
                  *args: str,
                  host='127.0.0.1',
@@ -166,12 +114,7 @@ class Connection(object):
 
     def reconnect(self):
         """关闭打开的服务器并重新启动"""
-        # self.close()
-        # self._db = pymysql.connect(**self._db_args)
-        # self._db.autocommit(True) # 二次确认
-
         self.close()
-
         self.db = pymongo.MongoClient(self._uri,
                                       tz_aware=True,
                                       serverSelectionTimeoutMS=self.timeoutMS,
@@ -179,7 +122,7 @@ class Connection(object):
                                       tzinfo=self.time_zone)
         self.set_database()
         self.set_collection()
-
+        # FIXME:
         self.session = self.db.start_session()
         self.sessionID = self.session.session_id
 
@@ -220,17 +163,6 @@ class Connection(object):
         # FIXME:应该放到游标迭代托管里
         return [i for i in self.cursor]
 
-    # def _ensure_connected(self):
-    #     # Mysql by default closes client connections that are idle for
-    #     # 8 hours, but the client library does not report this fact until
-    #     # you try to perform a query and it fails.  Protect against this
-    #     # case by preemptively closing and reopening the connection
-    #     # if it has been idle for too long (7 hours by default).
-    #     if (self._db is None
-    #             or (time.time() - self._last_use_time > self.max_idle_time)):
-    #         self.reconnect()
-    #     self._last_use_time = time.time()
-
     def _ensure_connected(self):
         # FIXME:
         """ 
@@ -244,10 +176,6 @@ class Connection(object):
                 or (time.time() - self._last_use_time > self.max_idle_time)):
             self.reconnect()
         self._last_use_time = time.time()
-
-    # def _cursor(self):
-    #     self._ensure_connected()
-    #     self._db.cursor()
 
     def _find(self, query):
         self.cursor = self.db_database_collection.find(query,
@@ -266,32 +194,6 @@ class Connection(object):
             self._cursor()
 
         return
-
-    # def execute(self, query, *parameters, **kwparameters):
-    #     """Executes the given query, returning the lastrowid from the query."""
-    #     cursor = self._cursor()
-    #     try:
-    #         cursor.execute(query, kwparameters or parameters)
-    #         return cursor.lastrowid
-    #     except Exception as e:
-    #         if e.args[0] == 1062:
-    #             pass
-    #         else:
-    #             traceback.print_exc()
-    #             raise e
-    #     finally:
-    #         cursor.close()
-
-    # insert = execute
-
-    # def get(self, query, *parameters, **kwparameters):
-    #     """返回查询结果"""
-    #     cursor = self._cursor()
-    #     try:
-    #         cursor.execute(query, kwparameters or parameters)
-    #         return cursor.fetchone()
-    #     finally:
-    #         cursor.close()
 
     def get(self, query: dict = None, limit=None):
         """返回查询结果的一条"""
@@ -316,62 +218,6 @@ class Connection(object):
             return result
         finally:
             cursor.close()
-
-    ## =============== high level method for table ===================
-
-    # def table_has(self, table_name, field, value):
-    #     if isinstance(value, str):
-    #         value = value.encode('utf8')
-    #     sql = 'SELECT %s FROM %s WHERE %s="%s"' % (field, table_name, field,
-    #                                                value)
-    #     d = self.get(sql)
-    #     return d
-
-    # def table_insert(self, table_name, item):
-    #     '''item is a dict : key is mysql table field'''
-    #     fields = list(item.keys())
-    #     values = list(item.values())
-    #     fieldstr = ','.join(fields)
-    #     valstr = ','.join(['%s'] * len(item))
-    #     for i in range(len(values)):
-    #         if isinstance(values[i], str):
-    #             values[i] = values[i].encode('utf8')
-    #     sql = 'INSERT INTO %s (%s) VALUES(%s)' % (table_name, fieldstr, valstr)
-    #     try:
-    #         last_id = self.execute(sql, *values)
-    #         return last_id
-    #     except Exception as e:
-    #         if e.args[0] == 1062:
-    #             # just skip duplicated item
-    #             pass
-    #         else:
-    #             traceback.print_exc()
-    #             print('sql:', sql)
-    #             print('item:')
-    #             for i in range(len(fields)):
-    #                 vs = str(values[i])
-    #                 if len(vs) > 300:
-    #                     print(fields[i], ' : ', len(vs), type(values[i]))
-    #                 else:
-    #                     print(fields[i], ' : ', vs, type(values[i]))
-    #             raise e
-
-    # def table_update(self, table_name, updates, field_where, value_where):
-    #     '''updates is a dict of {field_update:value_update}'''
-    #     upsets = []
-    #     values = []
-    #     for k, v in updates.items():
-    #         s = '%s=%%s' % k
-    #         upsets.append(s)
-    #         values.append(v)
-    #     upsets = ','.join(upsets)
-    #     sql = 'UPDATE %s SET %s WHERE %s="%s"' % (
-    #         table_name,
-    #         upsets,
-    #         field_where,
-    #         value_where,
-    #     )
-    #     self.execute(sql, *(values))
 
     def insert(self, query):
         """插入数据,传入字典为插入一条,传入列表为插入多条"""
@@ -401,28 +247,6 @@ class Connection(object):
 
 
 if __name__ == '__main__':
-    # db = Connection('localhost', 'db_name', 'user', 'password')
-    # 获取一条记录
-    # sql = 'select * from test_table where id=%s'
-    # data = db.get(sql, 2)
-
-    # # 获取多天记录
-    # sql = 'select * from test_table where id>%s'
-    # data = db.query(sql, 2)
-
-    # # 插入一条数据
-    # sql = 'insert into test_table(title, url) values(%s, %s)'
-    # last_id = db.execute(sql, 'test', 'http://a.com/')
-    # # 或者
-    # last_id = db.insert(sql, 'test', 'http://a.com/')
-
-    # 使用更高级的方法插入一条数据
-    # item = {
-    #     'title': 'test',
-    #     'url': 'http://a.com/',
-    # }
-    # last_id = db.table_insert('test_table', item)
-
     # 数据库管理
     connection = Connection()  # 连接mongo（默认数据库和集合）
     connection.set_database('demo')  # 重新设置数据库

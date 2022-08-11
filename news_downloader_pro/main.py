4b017411ac6f0e22ef2d8da3b8492f5f71522de4
Sancho <deerflores@gmail.com>
2022-08-11 12:16:41 +0800

    modified:   news_downloader_pro/__pycache__/db_manager.cpython-39.pyc 	new file:   news_downloader_pro/__pycache__/main_thread_copy.cpython-39-pytest-6.2.4.pyc 	modified:   news_downloader_pro/__pycache__/url_manager.cpython-39.pyc 	deleted:    news_downloader_pro/db_manager.py 	deleted:    news_downloader_pro/html_parser.py 	modified:   news_downloader_pro/main.py 	deleted:    news_downloader_pro/news_downloader_pro.log 	modified:   news_downloader_pro/news_downloader_pro.yml 	modified:   news_downloader_pro/news_downloader_pro_hubs.yml 	deleted:    news_downloader_pro/url_downloader.py 	deleted:    news_downloader_pro/url_manager.py

--- news_downloader_pro/main.py
+++ news_downloader_pro/main.py
@@ -1,150 +1,431 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-#FILE: main.py
-#CREATE_TIME: 2022-08-07
+#FILE: main_thread.py
+#CREATE_TIME: 2022-08-09
 #AUTHOR: Sancho
 """
-调度器(多线程、多进程、协程)
-抓取效率:946/分钟
+新闻爬虫
+pandas数据结构版
+效率：70条/分钟
+NOTE:在抓取网页和添加链接到网址池时耗费绝大部分时间
+- 访问网页需要等待
+- 创建pandas对象和更新浪费时间
 """
 
-import logging
-from logging.handlers import TimedRotatingFileHandler
-from pickle import NONE
+from statistics import mean
+import pandas
+import lzma
 import sys
 import time
-from tkinter.tix import Tree
-import requests
-import urllib.parse as urlparse
-from motor.motor_asyncio import AsyncIOMotorClient  # 异步操作mongo模块
-# import asyncio  # 异步操作文件读写
-# import aiohttp  # 异步操作网络请求
-import traceback
-import lzma
-import farmhash
 import yaml
-# import multiprocessing
-import url_manager
-import db_manager
-import url_downloader
-import html_parser
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import contextlib
+import pickle
+from pymongo.mongo_client import MongoClient
+import urllib.parse as urlparse
+import requests
+import re
+#TODO:
+from concurrent.futures import ThreadPoolExecutor
+# REVIEW:
+import traceback
 
-# import uvloop # 不适用windows
-# asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
-
-class NewsCrawler:
-    def __init__(self, concurrent_workers_max=30) -> None:
+class Loader:
+    def __init__(self) -> None:
         my_dir = sys.path[0]
         my_name = my_dir.split('\\')[-1]
         self.path = f'{my_dir}\\{my_name}'
-        self._concurrent_workers = 0
 
-        self.cfg = self._load_cfg()  # 读取配置文件
-        self.logger = self._init_file_logger(f'{self.path}.log')
-        self.db = self._login_db()  # 连接服务器
-        self.col = self.db.col
-        self.url_pool = url_manager.Url_Pool(self.path, self.db,
-                                             self.cfg)  # 读取链接池
+    def load_conf(self):
+        with open(f'{self.path}.yml', 'r', encoding='utf-8') as f:
+            conf = yaml.load(f, Loader=yaml.CLoader)
+        return conf
+
+    def _dump_hubs(self, hubs):
+        with contextlib.suppress(Exception):
+            with open(f'{self.path}_hubs.yml', 'w') as f:
+                yaml.dump(hubs, f)
+
+    def load_hubs(self):
+        with open(f'{self.path}_hubs.yml', 'r', encoding='utf-8') as f:
+            hubs = yaml.load(f, Loader=yaml.CLoader)
+            print(f'loading hubs:{hubs}')
+        return hubs
+
+    def _dump_cache(self, urls):
+        with contextlib.suppress(Exception):
+            with open(f'{self.path}.pkl', 'wb') as f:
+                pickle.dump(urls, f)
+                return True
+
+    def _load_cache(self):
+        with contextlib.suppress(Exception):
+            with open(f'{self.path}.pkl', 'rb') as f:
+                return pickle.load(f)
+
+
+class Mongo:
+    STATUS_FAILURE = b'0'
+    STATUS_SUCCESS = b'1'
+
+    def __init__(self, conf) -> None:
+        self.user = conf['user']
+        self.password = conf['password']
+        self.host = conf['host']
+        self.port = conf['port']
+        self.database = conf['database']
+        self.collection = conf['collection']
+
+        self._client()
+
+    def _client(self):
+        if self.user and self.password:
+            self.client = MongoClient(
+                f'mongodb://{self.user}:{self.password}@{self.host}:{self.port}'
+            )
+        else:
+            self.client = MongoClient(f'mongodb://{self.host}:{self.port}')
+
+        self.db = self.client[self.database]
+        self.coll = self.db[self.collection]
+        return True
+
+    def save_to_db(self, url, html, issave, status):
+        if status != 200 or issave == False:
+            return False
+        # 压缩数据
+        if isinstance(html, str):
+            html = html.encode('utf8')
+        html_lzma = lzma.compress(html)
+        good = False
+        try:
+            if self.coll.find_one({'url': url}):
+                print(f"failure save to db: {url}, status: {status}")
+                return good
+            self.coll.insert_one({
+                'url': url,
+                'status': status,
+                'html': html_lzma
+            })
+        except Exception as e:
+            if e.args[0] == 1062:
+                good = True
+            else:
+                traceback.print_exc()
+                raise e
+        return good
+
+
+class UrlManager:
+    def __init__(self, coll, conf) -> None:
+        self.coll = coll
+        self.conf = conf
+        self.columns_list = ['host', 'status', 'pendedtime', 'failure', 'mode']
+        self.url_pool = pandas.DataFrame([], columns=self.columns_list)
+
+    def _add(self, url_pool, url, mode):
+        if not isinstance(url, str):
+            return url_pool, False
+        # 检查子域名
+        host = urlparse.urlparse(url).netloc
+        if not host or '.' not in host:
+            print('bad url:', url)
+            return url_pool, False
+        # 是否存在网址池
+        if url in url_pool.index:
+            # 存在数据库中则不入库
+            if self.coll.find_one({'url': url
+                                   }) and url_pool.loc[url, 'mode'] == 'url':
+                return url_pool, False
+            # 已存在网址池且超过刷新时间，更新状态
+            refresh_mode = "pending_threshold" if mode == 'url' else "hub_refresh_span"
+            if time.time(
+            ) - url_pool.loc[url]["pendedtime"] > self.conf[refresh_mode]:
+                url_pool.loc[url]["status"] = "waiting"
+                url_pool.loc[url]["pendedtime"] = 0
+                return url_pool, True
+            # 已存在网址池且没有超过刷新时间，不更新
+            return url_pool, False
+        # 不存在网址池，添加
+        df = pandas.DataFrame([[host, "waiting", 0, 0, mode]],
+                              columns=self.columns_list,
+                              index=[url])
+        url_pool = url_pool.append(df)
+        return url_pool, True
+
+    def add_many(self, url_pool, urls: list, mode='url'):
+        if not urls:
+            return url_pool
+        elif isinstance(urls, str):
+            self._add(url_pool, urls, mode)
+            return url_pool
+        for url in urls:
+            url_pool, _ = self._add(url_pool, url, mode)
+        return url_pool
+
+    def _query(self, r, refresh_mode, box, box_count):
+        isrefresh = time.time() - r['pendedtime'] > self.conf[refresh_mode]
+        isout = len(box) < box_count
+        if isrefresh and isout:
+            box.append(r.name)
+            r['pendedtime'] = time.time()
+            r['status'] = 'pending'
+            return r, box, True
+        return r, box, False
+
+    def pop(self, url_pool, count, hub_percent=34, limit=True):
+        urls, hubs = [], []
+        # 计算需求量
+        hubs_count = count * hub_percent // 100
+        urls_count = count - hubs_count
+        # 遍历网址池
+        for _, group in url_pool.groupby('host'):
+            for url, r in group.iterrows():
+                if r["mode"] == 'hub':
+                    r, hubs, sign = self._query(r, 'hub_refresh_span', hubs,
+                                                hubs_count)
+                elif r["mode"] == 'url':
+                    urls_count = count - hubs_count  # 动态调整需求量
+                    r, urls, sign = self._query(r, 'pending_threshold', urls,
+                                                urls_count)
+                if limit and sign:
+                    break
+            if len(hubs) + len(urls) >= count:
+                break
+        return hubs + urls, url_pool
+
+    def set_status(self, url_pool, redirected_url, url, status_code):
+        """访问链接后设置状态"""
+        # 如果返回链接和链接不相等，采用返回链接
+        if redirected_url != url:
+            url = redirected_url
+        # 简单判断主机地址是否合法
+        host = urlparse.urlparse(url).netloc  # 解析到主机地址
+        if not host or '.' not in host:
+            print('地址错误:', url, ', len of ur:', len(url))
+            return url_pool, False
+        # 更新链接状态
+        if url not in url_pool.index:  # 如果不存在网址池则不更新此链接
+            return url_pool, False
+        if status_code == 200:  # 如果成功访问，则从网址池移除，并标记在数据库中记录
+            if url_pool.loc[url]["mode"] == 'url':
+                url_pool = url_pool.drop([url])
+                return url_pool, True
+            # 如果成功访问，更新hub链接的状态
+            url_pool.loc[url]['status'] = 'waiting'
+            url_pool.loc[url]['pendedtime'] = time.time()
+            return url_pool, True
+        elif status_code == 404:  # 如果地址不存在，则从网址池移除，并标记在数据库中记录
+            url_pool = url_pool.drop([url])
+            return url_pool, True
+        elif url_pool.loc[url]['failure'] > 0:  # 访问失败时
+            url_pool.loc[url]['failure'] += 1  # 记录失败次数+1
+            if url_pool.loc[url]["mode"] == 'url' and url_pool.loc[url][
+                    'failure'] > self.conf['failure_threshold']:  # 如果失败次数超过上限
+                url_pool = url_pool.drop([url])  # 从网址池移除
+                return url_pool, True  # 标记在数据库中记录
+            else:  # 如果失败次数没有超过上限,则修改状态
+                url_pool.loc[url]['status'] = 'waiting'
+        else:  # 第一次失败，则修改状态
+            url_pool.loc[url]['failure'] = 1  # 记录失败次数
+            url_pool.loc[url]['status'] = 'waiting'
+        return url_pool, False
+
+
+class Downloader:
+    def __init__(self) -> None:
+        pass
+
+    def fetch(self, session, url, headers=None, timeout=9):
+        # TODO:UA池
+        _headers = headers or {
+            'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        }
+
+        try:
+            resp = session.get(url, headers=_headers, timeout=timeout)
+            status = resp.status_code
+            self.encoding = "utf-8"
+            html = resp.text
+            redirected_url = str(resp.url)
+            print(f'succes download:{url} | status:{status}')
+        except Exception as e:
+            msg = f'Failed download: {url} | exception: {str(type(e))}, {str(e)}'
+            print(msg)
+            html = ''
+            status = 0
+            redirected_url = url
+        return status, html, redirected_url
+
+
+class Parser:
+    def __init__(self) -> None:
+        self.G_BIN_POSTFIX = ('exe', 'doc', 'docx', 'xls', 'xlsx', 'ppt',
+                              'pptx', 'pdf', 'jpg', 'png', 'bmp', 'jpeg',
+                              'gif', 'zip', 'rar', 'tar', 'bz2', '7z', 'gz',
+                              'flv', 'mp4', 'avi', 'wmv', 'mkv', 'apk')
+        self.G_NEWS_POSTFIX = ('.html?', '.htm?', '.shtml?', '.shtm?')
+        self.G_PATTERN_TAG_A = re.compile(
+            r'<a[^>]*?href=[\'"]?([^> \'"]+)[^>]*?>(.*?)</a>',
+            re.I | re.S | re.M)
+
+    def _clean_url(self, url):
+        # 1. 是否为合法的http url
+        if not url.startswith('http'):
+            return ''
+        # 2. 去掉静态化url后面的参数
+        for np in self.G_NEWS_POSTFIX:
+            p = url.find(np)
+            if p > -1:
+                p = url.find('?')
+                url = url[:p]
+                return url
+        # 3. 不下载二进制类内容的链接
+        up = urlparse.urlparse(url)
+        path = up.path
+        if not path:
+            path = '/'
+        postfix = path.split('.')[-1].lower()
+        if postfix in self.G_BIN_POSTFIX:
+            return ''
+        # 4. 去掉标识流量来源的参数
+        # badquery = ['spm', 'utm_source', 'utm_source', 'utm_medium', 'utm_campaign']
+        good_queries = []
+        for query in up.query.split('&'):
+            qv = query.split('=')
+            if qv[0].startswith('spm') or qv[0].startswith('utm_'):
+                continue
+            if len(qv) == 1:
+                continue
+            good_queries.append(query)
+        query = '&'.join(good_queries)
+        url = urlparse.urlunparse((
+            up.scheme,
+            up.netloc,
+            path,
+            up.params,
+            query,
+            ''  #  crawler do not care fragment
+        ))
+        return url
+
+    def _filter_good(self, urls, hosts):
+        goodlinks = []
+        for url in urls:
+            host = urlparse.urlparse(url).netloc
+            if host in hosts:
+                goodlinks.append(url)
+        return goodlinks
+
+    def _extract_links_re(self, url, html):
+        """使用re模块从hub页面提取链接"""
+        newlinks = set()
+        aa = self.G_PATTERN_TAG_A.findall(html)
+        for a in aa:
+            link = a[0].strip()
+            if not link:
+                continue
+            link = urlparse.urljoin(url, link)
+            if link := self._clean_url(link):
+                newlinks.add(link)
+        # print("add:%d urls" % len(newlinks))
+        return newlinks
+
+    def process(self, status, html, redirected_url, ishub, hosts):
+        # 提取hub网页中的链接, 新闻网页中也有“相关新闻”的链接，按需提取
+        if status != 200:
+            return False
+        if ishub:
+            newlinks = self._extract_links_re(redirected_url, html)
+            goodlinks = self._filter_good(newlinks, hosts)
+            print(f"{len(goodlinks)}/{len(newlinks)}, goodlinks/newlinks")
+            return set(goodlinks)
+
+
+class Crawler:
+    def __init__(self) -> None:
+        # 读取配置文件
+        self.loader = Loader()
+        self.conf = self.loader.load_conf()
+        self.hubs = self.loader.load_hubs()
+        # 连接数据库
+        self.mongo = Mongo(self.conf)
+        self.db = self.mongo.db
+        self.coll = self.mongo.coll
+        # 载入网址池
+        self.url_manager = UrlManager(self.coll, self.conf)
+        self.url_pool = self.url_manager.url_pool
+        self.url_pool = self.url_manager.add_many(self.url_pool, self.hubs,
+                                                  "hub")
+        cache = self.loader._load_cache()
+        self.url_pool = self.url_manager.add_many(self.url_pool, cache)
+        # 其它功能
+        self.downloader = Downloader()
+        self.parser = Parser()
 
     def close(self):
-        self.url_pool.close()
+        # 写入配置文件
+        # self.loader._dump_hubs(self.hubs)
+        # self.loader._dump_cache(self.urls)
+        # 关闭数据库
+        self.mongo.client.close()
+        # 程序退出
         sys.exit()
 
-    def _load_cfg(self):
-        with open(f'{self.path}.yml', 'r', encoding='utf-8') as f:
-            cfg = yaml.load(f, Loader=yaml.CLoader)
-        return cfg
+    def __del__(self):
+        self.close()
 
-    def _init_file_logger(self, fname):
-        ch = TimedRotatingFileHandler(
-            fname, when="midnight")  # 将日志消息发送到磁盘文件，并支持日志文件按时间切割
-        ch.setLevel(logging.INFO)  # 默认警报等级
-        fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'  # 格式化
-        formatter = logging.Formatter(fmt)
-        ch.setFormatter(formatter)  # 添加格式化到配置
-        logger = logging.getLogger(fname)  # 获取日志对象
-        logger.addHandler(ch)  # 添加配置到记录器
-        return logger
-
-    def _login_db(self):
-        return db_manager.DB(self.cfg, self.logger)
-
-    def _print_info(self):
-        if self._concurrent_workers > MAX_WORKERS_CONCURRENT:
-            print('====== got workers_max, sleep 1 sec to next worker =====')
-            time.sleep(1)
-        gap = time.time() - self.last_rating_time
-        if gap > 5:
-            rate = self.counter / gap
-            print('\tloop_crawl() rate:%s, counter: %s, workers: %s' %
-                  (round(rate, 2), self.counter, self._concurrent_workers))
-            self.last_rating_time = time.time()
-            self.counter = 0
-
-    def _if_refurbish(self):
-        if time.time() - self.last_loading_time > 300:
-            self.cfg = self._load_cfg()
-            self.last_loading_time = time.time()  # 更新时间
-            if self.cfg['exit'] == True:  # 退出程序
+    def _re_load_conf(self, last_loading_time, refresh_time=300):
+        if time.time() - last_loading_time > refresh_time:
+            # 读取配置信息
+            self.conf = self.loader.load_conf()
+            # 退出检测
+            if self.conf['exit'] == True:
                 self.close()
+            return time.time()
+        return last_loading_time
 
-    def loop_crawl(self):
-        self.last_rating_time = time.time()
-        self.last_loading_time = time.time()
-        self.counter = 0
-        while 1:
-            self._print_info()
-            self._if_refurbish()
-            # with ThreadPoolExecutor() as thread_pool:
-            #     pass
-            # 读取待下载链接
-            tasks = self.url_pool.pop(MAX_WORKERS_CONCURRENT,
-                                      self.cfg['hub_refresh_span'],
-                                      self.cfg['pending_threshold'])
+    def crawl(self):
+        last_loading_time = time.time()
+        self._concurrent_workers = 0
+        while True:
+            # 刷新配置文件
+            last_loading_time = self._re_load_conf(
+                last_loading_time)  # 重新获取配置信息
+            # 取出待下载链接
+            tasks, self.url_pool = self.url_manager.pop(
+                self.url_pool, MAX_WORKERS_CONCURRENT)
             if not tasks:
                 print('no url to crawl, sleep')
                 time.sleep(1)
                 continue
+            # 遍历下载链接
+            for url in tasks:
+                with requests.session() as session:
+                    status, html, redirected_url = self.downloader.fetch(
+                        session, url)  # 抓取网页
+                    self.url_pool, ifsave = self.url_manager.set_status(
+                        self.url_pool, redirected_url, url, status)  # 设置状态
+                    if ishub := url in self.url_pool[self.url_pool['mode'] ==
+                                                     'hub'].index:
+                        links = self.parser.process(
+                            status, html, redirected_url, ishub,
+                            set(self.url_pool['host'].tolist()))  # 解析数据
+                        self.url_pool = self.url_manager.add_many(
+                            self.url_pool, links)
+                    else:
+                        self.mongo.save_to_db(url, html, ifsave, status)
 
-            with requests.session() as session:
-                with ThreadPoolExecutor(MAX_WORKERS_THREADING) as thread_pool:
-
-                    self._concurrent_workers += 1
-                    self.counter += 1
-                    [
-                        thread_pool.submit(html_parser.process, session, url,
-                                           ishub, self.url_pool, self.db)
-                        for url, ishub in tasks
-                    ]
-                    self._concurrent_workers -= 1
-
-            # for url, ishub in tasks:
-            #     self._concurrent_workers += 1
-            #     self.counter += 1
-            #     session = requests.session()
-            #     html_parser.process(session, url, ishub, self.url_pool,
-            #                         self.db)
-            #     self._concurrent_workers -= 1  # FIXME:此计数器未生效
-            #     session.close()
-
-    def run(self):  # sourcery skip: remove-redundant-pass
+    def run(self):
         try:
-            # 创建异步任务
-            # self.loop = asyncio.get_event_loop()
-            # self.loop.run_until_complete(self.loop_crawl())
-            self.loop_crawl()
+            self.crawl()
         except KeyboardInterrupt:
             print('stopped by yourself!')
             self.close()
-            pass
 
 
 if __name__ == '__main__':
-    MAX_WORKERS_THREADING = 12
-    MAX_WORKERS_PROCESSING = 6
-    MAX_WORKERS_CONCURRENT = 24
-    nc = NewsCrawler()
-    nc.run()
-    print("done!")
+    MAX_WORKERS_PROCESS, MAX_WORKERS_THREAD, MAX_WORKERS_CONCURRENT = 6, 12, 24
+    news_crawler = Crawler()
+    news_crawler.run()
